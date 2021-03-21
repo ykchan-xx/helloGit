63365347b2f0ded693dc55dd912c6914a4c1a112
ykchan-xx <379159353@qq.com>
2021-03-21 15:58:43 +0800

    my first use git with idea

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * Created by yongkang丶chen on 2021/3/21.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
